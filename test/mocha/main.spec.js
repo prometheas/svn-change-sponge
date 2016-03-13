263791e670bbed4a04733ed3f25e6c8d08c7184c
John Lianoglou <john.lianoglou@viacom.com>
2016-03-13 03:42:30 -0400

    Refined dependencies and added detection of new files
    After grappling with how to make async callbacks synchronous for ages, I finally managed to get `svn-interface` to work synchronously for me, and I managed to implement a capability to detect new / unknown files in a wc dir.

--- test/mocha/main.spec.js
+++ test/mocha/main.spec.js
@@ -3,19 +3,57 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var util = require('util');
+var dircompare = require('dir-compare');
+var path = require('path');
 var shell = require('shelljs');
+var svn = require('svn-interface');
+var temp = require('temp');
 
 var sponge = require('../../lib/main.js');
+var repoHost = 'svn';
 
-describe('SvnChangeSponge', function () {
 
-  var testPath = '/tmp/testing';
-  var repoUrl = 'http://svn/';
+/**
+ * Generates a Subversion URL from a path.
+ *
+ * @param {string} svnPath
+ *
+ * @return {string}
+ */
+function getSvnUrl(svnPath) {
+  if (! svnPath.match(/^\//)) {
+    throw 'svnPath param must start with "/"';
+  }
 
-  before(function () {
-    shell.exec(util.format('mkdir -p "%s"; cd "%s"; rm -rf *', testPath, testPath));
-    shell.exec('svn co http://svn/');
+  return 'http://' + repoHost + svnPath;
+}
+
+/**
+ * Answers whether the identified wc directory matches the HEAD revision
+ * in the Subversion repository.
+ *
+ * @return {boolean}
+ */
+function doesWcMatchRepoHead() {
+  var results;
+  var repoTemp = path.join(temp.mkdirSync('repo-state-'), 'wc');
+  svn.checkout(getSvnUrl('/'), repoTemp);
+
+  results = dircompare.compareSync(testWcPath, repoTemp, {
+    compareContent: true,
+    excludeFilter: '.svn'
+  });
+
+  return results.differences === 0;
+}
+
+
+describe('SvnPusher', function () {
+  /** @var testWcPath string the path of the testing root dir */
+  var testWcPath = path.join(temp.mkdirSync('testing'), 'wc');
+
+  before(function (done) {
+    svn.checkout(getSvnUrl('/'), testWcPath, done);
   });
 
   describe('the module', function () {
@@ -23,14 +61,30 @@ describe('SvnChangeSponge', function () {
       expect(sponge).to.be.a('object');
     });
 
-    it('should have a function called #absorbChanges', function () {
-      expect(sponge.absorbChanges).to.be.a('function');
+    it('should have a function called #pushChanges', function () {
+      expect(sponge.pushChanges).to.be.a('function');
     });
   });
 
-  describe('ability to validate Subversion wc directory', function () {
-    it('should throw an error when pointed at a non-Subversion wc directory', function () {
+  describe.skip('ability to validate Subversion wc directory', function () {
+    // coming soon
+  });
 
+  describe('wc change detection', function () {
+    it('should be able to detect new files', function () {
+      expect(sponge.hasNewFiles(testWcPath)).to.be.false;
+      shell.touch(path.join(testWcPath, 'foo.txt'));
+      expect(sponge.hasNewFiles(testWcPath)).to.be.true;
+    });
+  });
+
+  describe.skip('ability to push basic changes to repo', function () {
+    it('should be able to detect, add, and commit a new file', function () {
+      shell.touch(path.join(testWcPath, 'foo.txt'));
+      expect(doesWcMatchRepoHead()).to.be.false;
+
+      sponge.pushChanges();
+      expect(doesWcMatchRepoHead()).to.be.true;
     });
   });
 });
